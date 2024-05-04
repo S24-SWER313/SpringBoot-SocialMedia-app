cc0c05211a5fe9f16570d52ac28638084ccf23a4
zainab <engzainabatwa1@gmail.com>
2024-05-04 14:08:26 +0300

    nn

--- src/main/java/com/project/proo/postInfo/PostController.java
+++ src/main/java/com/project/proo/postInfo/PostController.java
@@ -38,6 +38,7 @@ import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.Link;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
